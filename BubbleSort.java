91e5539705854206607819290af4ce4e21989463
Om Nepal <nepalom15@gmail.com>
2025-06-22 12:13:34 -0500

    add a isSorted flag to optimize the bubble sort for times when the array gets sorted earlier

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,7 +1,9 @@
 public class BubbleSort {
     public static void main(String[] args) {
         int[] nums = {1, 7, 9, 2, 4, 3};
+        //int[] nums = {1, 2, 3, 4, 5, 6};
         int temp;
+        boolean isSorted;
 
         System.out.println("before sorting");
         for (int num : nums) {
@@ -11,14 +13,24 @@ public class BubbleSort {
 
         for (int i = 0; i < nums.length - 1; i++) { //nums.length - 1 bcoz for every completion of loop, we get one element sorted, so after n-1 times, n-1 elements are sorted which means the remaining one element is also sorted.
             //System.out.println("steps i " + i);
+            isSorted = true;
             for (int j = 0; j < nums.length - i - 1; j++) { //since after every loop, last element is sorted so we don't need to check that element that's why we keep subtracting i each time
-                //System.out.println("steps: " + j);
+                //System.out.println("steps: " + +j);
                 if (nums[j] > nums[j+1]) {
                     temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
+                    isSorted = false;
                 }
             }
+
+            for (int num : nums) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+
+            if (isSorted)
+                break;
 
         }
 
