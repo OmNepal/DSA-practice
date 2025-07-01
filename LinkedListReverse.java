bc80267580c5078d3d7b8f79c48097df8582ad5e
Om Nepal <nepalom15@gmail.com>
2025-06-30 23:48:16 -0500

    add method to reverse Linked List iteratively using 3 pointer: prev, curr and next

---
+++ LinkedListReverse.java
@@ -0,0 +1,75 @@
+
+public class LinkedListReverse {
+    Node head;
+
+    class Node {
+        Integer data;
+        Node next;
+
+        Node(Integer data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    void insertAtFirst(Integer data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void printList() {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+        System.out.println();
+    }
+
+    void reverseListIterative() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        LinkedListReverse list = new LinkedListReverse();
+        list.insertAtFirst(3);
+        list.insertAtFirst(2);
+        list.insertAtFirst(8);
+        list.insertAtFirst(3);
+        list.insertAtFirst(7);
+        list.insertAtFirst(5);
+        list.insertAtFirst(1);
+
+        System.out.println("Original List: ");
+        list.printList();
+
+        list.reverseListIterative();
+        System.out.println("Reversed List: ");
+        list.printList();
+
+
+    }
+}
