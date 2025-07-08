36cadb518b1b4de5228015a7b36dba9aeb54a506
Om Nepal <nepalom15@gmail.com>
2025-07-07 20:45:16 -0500

    add inOrderTraversal method to traverse and print the tree in the in order way

--- BinaryTreePractice.java
+++ BinaryTreePractice.java
@@ -35,17 +35,32 @@ class BinaryTreePractice {
             preOrderTraversal(root.right);
 
         }
+
+        public static void inOrderTraversal(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrderTraversal(root.left);
+            System.out.print(root.data +" ");
+            inOrderTraversal(root.right);
+        }
     }
 
     public static void main(String[] args) {
-        int nodes[] = {7, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
 
-        System.out.println("Root" + root.data);
+        System.out.println("Root: " + root.data);
 
+        System.out.print("Pre order traversal: ");
         tree.preOrderTraversal(root);
+
+        System.out.println();
+
+        System.out.print("In order traversal: ");
+        tree.inOrderTraversal(root);
     }
 
 }
