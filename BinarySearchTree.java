f3d791af2f370d40c78ea08664ec17466c3dca8f
Om Nepal <nepalom15@gmail.com>
2025-07-14 17:22:25 -0500

    add a recursive method to delete given value in the Binary Search Tree based on 3 different cases

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,4 +1,4 @@
-class BinarySearchTree {
+ class BinarySearchTree {
     static class Node {
         int data;
         Node left;
@@ -36,18 +36,68 @@ class BinarySearchTree {
 
     }
 
-    public static void main(String[] args) {
+    public static Node delete(int val, Node root) {
+       /* if (root == null) {
+            return null;
+        }*/
+        if (val < root.data) {
+            root.left = delete(val, root.left);
+        } else if (val > root.data) {
+            root.right = delete(val, root.right);
+        } else { //root.data = val
+            //Case 1: no child
+            if (root.left == null && root.right == null)
+                return null;
+
+            //Case 2: One child
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            //Case 3: Two child
+            Node inOrderSuccessor = inOrderSuccessor(root.right); //look for the inOrderSuccessor in the right subtree
+            root.data = inOrderSuccessor.data; //put inOrderSuccessor in the root's position
+            root.right = delete(inOrderSuccessor.data, root.right); //delete inOrderSuccessor from its previous position
+        }
+        return root;
+    }
+
+     private static Node inOrderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+     }
+
+
+     public static void main(String[] args) {
         int values[] = {5,1,3,4,2,7};
         Node root = null;
         for (int i : values) {
             root = BinarySearchTree.BST(i, root);
         }
-        System.out.println(root.data);
-        System.out.print(root.left.data + " ");
-        System.out.println(root.right.data);
-        System.out.println(root.left.right.data);
+        System.out.println("        " + root.data); // 5
+        System.out.print("    " + root.left.data + "       "); // 1
+        System.out.println(root.right.data); // 7
+        System.out.println("      " + root.left.right.data); // 3
+        System.out.print("    " + root.left.right.left.data  + "   "); // 2
+        System.out.println(root.left.right.right.data); // 4
+
+         System.out.println();
 
         boolean result = BinarySearchTree.searchBST(7, root);
-        System.out.println(result);
+        System.out.println(result ? "7 is in the tree" : "7 is not in the tree");
+
+        root = BinarySearchTree.delete(3, root);
+
+         System.out.println("        " + root.data); // 5
+         System.out.print("    " + root.left.data + "       "); // 1
+         System.out.println(root.right.data); // 7
+         System.out.println("      " + root.left.right.data); // 4 takes 3's place
+         System.out.print("    " + root.left.right.left.data  + "   "); // 2
+         //System.out.println(root.left.right.right.data); //  null instead of 4
+
+
     }
 }
