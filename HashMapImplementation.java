98658bed52ad51dd7222532a781606feabceb58c
Om Nepal <nepalom15@gmail.com>
2025-07-22 23:28:28 -0500

    create a HashMap class and general structure needed to implement HashMap from scratch

---
+++ HashMapImplementation.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class HashMapImplementation {
+    public static class HashMap<K, V> {
+        static int n;
+        static int N;
+        static LinkedList[] buckets;
+
+        class Node {
+            K key;
+            V value;
+
+            Node (K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        void put(K key, V value) {
+
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
