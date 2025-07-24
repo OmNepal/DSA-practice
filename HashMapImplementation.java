9729de7e0b5a5e08994677ada81a43129d2dc641
Om Nepal <nepalom15@gmail.com>
2025-07-23 23:44:17 -0500

    add put method to add new key-value pair to the HashMap

--- HashMapImplementation.java
+++ HashMapImplementation.java
@@ -4,7 +4,13 @@ public class HashMapImplementation {
     public static class HashMap<K, V> {
         static int n;
         static int N;
-        static LinkedList[] buckets;
+        static LinkedList[] buckets = new LinkedList[4];
+
+        HashMap() {
+            for (int i = 0; i < buckets.length; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+        }
 
         class Node {
             K key;
@@ -16,11 +22,34 @@ public class HashMapImplementation {
             }
         }
 
-        void put(K key, V value) {
-
+        int hash(K key){
+            return 0;
         }
+
+        private int searchLL(K key) {
+            return 0;
+        }
+
+        void put(K key, V value) {
+            int bi = hash(key);
+            int di = searchLL(key);
+            LinkedList ll = buckets[bi];
+            if (ll.contains(key)) {
+                Node node = (Node) ll.get(di);
+                node.value = value;
+            } else {
+                ll.add(new Node(key, value));
+            }
+        }
+
+
     }
     public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap();
+        map.put("om", 19);
 
+        for (LinkedList ll : map.buckets) {
+            System.out.println();
+        }
     }
 }
